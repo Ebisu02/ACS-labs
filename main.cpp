8577f200b2d1260ed953b6985a184fa89242b4b7
Sergey Ososov <helloimclinker@gmail.com>
2022-10-15 19:26:46 +0700

    +ram test

--- main.cpp
+++ main.cpp
@@ -55,76 +55,100 @@ double meanSquareDeviation(vector<T> array, int n, char combination) {
     return deviation;
 }
 
+void RAM_TEST(int TEST_AMOUNTOFLAUNCHES) {
+    vector<long double> w_times;
+    vector<long double> r_times;
+    benchDisk* TESTS = new benchDisk[TEST_AMOUNTOFLAUNCHES];
+    size_t CONTAINER_SIZE = 1024 * 8; // 8 MB
+    for (int i = 0; i < TEST_AMOUNTOFLAUNCHES; ++i) {
+        vector<int> d(CONTAINER_SIZE / 4, 0);
+        TESTS[i].MemoryType = "RAM";
+        TESTS[i].BlockSize = CONTAINER_SIZE / 1024;
+        TESTS[i].ElementType = "INT";
+        TESTS[i].BufferSize = sizeof(d[0]);
+        TESTS[i].LaunchNum = i + 1;
+        TESTS[i].Timer = "CHRONO";
+        //WRITE
+        auto w_tnow = std::chrono::high_resolution_clock::now();
+        try {
+            ofstream fout("test" + to_string(i) + ".txt");
+            for (auto& j: d) {
+                fout << j << "\n";
+            }
+            fout.close();
+        }
+        catch (exception e) {
+            printf(e.what());
+            exit(-1);
+        }
+        auto w_time = std::chrono::duration_cast<std::chrono::nanoseconds>(
+                std::chrono::high_resolution_clock::now() - w_tnow);
+        TESTS[i].WriteTime = w_time.count() * 1e-9;
+        w_times.push_back(w_time.count() * 1e-9);
+        //cout << TESTS[i].WriteTime << " sec\n";
+        long double avg = 0.0;
+        for (int j = 0; j < i; ++j) {
+            avg += TESTS[j].WriteTime;
+        }
+        TESTS[i].AverageWriteTime = avg / TESTS[i].LaunchNum;
+        TESTS[i].WriteBandWidth = ((CONTAINER_SIZE / 1024) / TESTS[i].AverageWriteTime); // *1e6
+        TESTS[i].AbsErrorWrite = meanSquareDeviation(w_times, w_times.size(), 'g');
+        TESTS[i].RelErrorWrite = TESTS[i].AbsErrorWrite / TESTS[i].AverageWriteTime * 100;
+        // READ
+        auto r_tnow = std::chrono::high_resolution_clock::now();
+        try {
+            ifstream fin("test" + to_string(i) + ".txt", ios::app);
+            string line;
+            while (getline(fin, line)) {
+                line.clear();
+            }
+            fin.close();
+        }
+        catch (exception e) {
+            printf(e.what());
+            exit(-1);
+        }
+        auto r_time = std::chrono::duration_cast<std::chrono::nanoseconds>(
+                std::chrono::high_resolution_clock::now() - w_tnow);
+        TESTS[i].ReadTime = r_time.count() * 1e-9;
+        r_times.push_back(r_time.count() * 1e-9);
+        //cout << TESTS[i].ReadTime << "\n";
+        avg = 0.0;
+        for (int j = 0; j < i; ++j) {
+            avg += TESTS[i].ReadTime;
+        }
+        TESTS[i].AverageReadTime = avg / TESTS[i].LaunchNum;
+        TESTS[i].ReadBandWidth = ((CONTAINER_SIZE / 1024) / TESTS[i].AverageReadTime);
+        TESTS[i].AbsErrorRead = meanSquareDeviation(r_times, r_times.size(), 'g');
+        TESTS[i].RelErrorRead = TESTS[i].AbsErrorRead / TESTS[i].AverageReadTime * 100;
+        cout << TESTS[i].RelErrorRead << "  " << TESTS[i].RelErrorWrite << "\n";
+    }
+}
+
+void FLASH_TEST(int TEST_AMOUNTOFLAUNCHES) {
+    return;
+}
+
+void SSD_TEST(int TEST_AMOUNTOFLAUNCHES) {
+    return;
+}
+
 int main() {
     std::iostream::sync_with_stdio(false);
     string TEST_TYPE;
     long int TEST_AMOUNTOFLAUNCHES;
     cout << "Enter a params for test like this (without \"[]\"):\n"
             "[Test type(RAM/FLASH/SSD)] [10 (Amount of launches)]\n";
-    vector<long double> w_times;
-    vector<long double> r_times;
     cin >> TEST_TYPE >> TEST_AMOUNTOFLAUNCHES;
     if (TEST_TYPE == "RAM" || TEST_TYPE == "FLASH" || TEST_TYPE == "SSD") {
-        benchDisk* TESTS = new benchDisk[TEST_AMOUNTOFLAUNCHES];
-        size_t CONTAINER_SIZE = 1024 * 8; // 8 MB
-        for (int i = 0; i < TEST_AMOUNTOFLAUNCHES; ++i, CONTAINER_SIZE *= 2) {
-            vector<int> d(CONTAINER_SIZE / 4, 0);
-            TESTS[i].MemoryType = TEST_TYPE;
-            TESTS[i].BlockSize = CONTAINER_SIZE / 1024;
-            TESTS[i].ElementType = "INT";
-            TESTS[i].BufferSize = sizeof(d[0]);
-            TESTS[i].LaunchNum = i + 1;
-            TESTS[i].Timer = "CHRONO";
-            //WRITE
-            auto w_tnow = std::chrono::high_resolution_clock::now();
-            try {
-                ofstream fout("test" + to_string(i) + ".txt");
-                for (auto& j: d) {
-                    fout << j << "\n";
-                }
-                fout.close();
-            }
-            catch (exception e) {
-                printf(e.what());
-                exit(-1);
-            }
-            auto w_time = std::chrono::duration_cast<std::chrono::nanoseconds>(
-                    std::chrono::high_resolution_clock::now() - w_tnow);
-            TESTS[i].WriteTime = w_time.count() * 1e-9;
-            //cout << TESTS[i].WriteTime << " sec\n";
-            long double avg = 0.0;
-            for (int j = 0; j < i; ++j) {
-                avg += TESTS[j].WriteTime;
-            }
-            TESTS[i].AverageWriteTime = avg / TESTS[i].LaunchNum;
-            TESTS[i].WriteBandWidth = ((CONTAINER_SIZE / 1024) / TESTS[i].AverageWriteTime); // *1e6
-            TESTS[i].AbsErrorWrite = meanSquareDeviation(d, d.size(), 'g');
-            TESTS[i].RelErrorWrite = TESTS[i].AbsErrorWrite / TESTS[i].AverageWriteTime * 100;
-            // READ
-            auto r_tnow = std::chrono::high_resolution_clock::now();
-            try {
-                ifstream fin("test" + to_string(i) + ".txt", ios::app);
-                string line;
-                while (getline(fin, line)) {
-                    line.clear();
-                }
-                fin.close();
-            }
-            catch (exception e) {
-                printf(e.what());
-                exit(-1);
-            }
-            auto r_time = std::chrono::duration_cast<std::chrono::nanoseconds>(
-                    std::chrono::high_resolution_clock::now() - w_tnow);
-            TESTS[i].ReadTime = r_time.count() * 1e-9;
-            //cout << TESTS[i].ReadTime << "\n";
-            avg = 0.0;
-            for (int j = 0; j < i; ++j) {
-                avg += TESTS[i].ReadTime;
-            }
-            TESTS[i].AverageReadTime = avg / TESTS[i].LaunchNum;
-            TESTS[i].ReadBandWidth = ((CONTAINER_SIZE / 1024) / TESTS[i].AverageReadTime);
-            TESTS[i]
+        if (TEST_TYPE == "RAM") {
+            RAM_TEST(TEST_AMOUNTOFLAUNCHES);
+        }
+        if (TEST_TYPE == "FLASH") {
+            FLASH_TEST(TEST_AMOUNTOFLAUNCHES);
+        }
+        if (TEST_TYPE == "SSD") {
+            SSD_TEST(TEST_AMOUNTOFLAUNCHES);
         }
     }
     else {
